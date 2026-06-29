6dbdf498c94c340d9e11fccd9787b99913f20499
Shayana Shrestha <sshres25@vols.utk.edu>
2026-06-29 12:06:49 -0400

    Reverse number - JS

---
+++ JS/reverse_numbers.js
@@ -0,0 +1,17 @@
+"use strict";
+
+function reverseNumber(n) {
+    console.log (n.toString().split("").reverse().join(""));
+}
+
+function reverseNegNumber(n) {
+    console.log (parseFloat(n.toString().split("").reverse().join("")) * Math.sign(n));
+
+}
+
+reverseNumber(12345);
+reverseNumber(35);
+reverseNumber(10494931);
+
+reverseNegNumber(-12);
+reverseNegNumber(-123456789);
