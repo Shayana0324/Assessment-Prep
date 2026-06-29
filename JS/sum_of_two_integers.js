a035c04639b98f46b9e8700e3e41859da86bb867
Shayana Shrestha <sshres25@vols.utk.edu>
2026-06-29 11:23:56 -0400

    Sum of two integers - JS

---
+++ JS/sum_of_two_integers.js
@@ -0,0 +1,14 @@
+"use strict";
+
+function solution(n) {
+    let first_num = Math.floor(n / 10);
+    let modulo = n % 10;
+    console.log(first_num);
+    console.log(modulo);
+    let sum = first_num + modulo;
+    console.log("Sum of two integers = " + sum);
+
+}
+
+solution(34);
+
