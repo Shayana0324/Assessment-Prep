c8ffeb0c56b6c47cc00c446856618bac7ce0c7af
Shayana Shrestha <shayana0324@gmail.com>
2026-06-29 15:21:57 -0400

    Sqaure and cube of a number - js

---
+++ JS/square_and_cube.js
@@ -0,0 +1,20 @@
+// Finding the square and cube of a number
+"use strict";
+
+const a = 3;
+
+// Square of a number
+function numSquare(a) {
+    let sqr = a ** 2;
+    console.log("Sqaure = " + sqr);
+}
+
+numSquare(a);
+
+// Cube of a number
+function numCube(a) {
+    let cube = a ** 3;
+    console.log("Cube = " + cube);
+}
+
+numCube(a);
