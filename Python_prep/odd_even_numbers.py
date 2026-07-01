687035ce995a58af47f92dd4df754bcc484a3fd8
Shayana Shrestha <shayana0324@gmail.com>
2026-06-30 22:14:07 -0400

    Odd and even numbers - python

---
+++ Python_prep/odd_even_numbers.py
@@ -0,0 +1,9 @@
+def evenOddNum():
+    arr = [1, 2, 3, 4, 5, 6 ]
+    for i in arr:
+        if i % 2 == 0:
+            print("Even Numbers:", i)
+        else:
+            print("Odd Numbers:", i)
+
+evenOddNum()
